61cad38ce481eb2a30d98483f9187dfe018d110e
jaclynflomen <j_floman@fanshaweonline.ca>
2019-03-27 12:06:12 -0400

    adding socket stuff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var http = require('http').Server(app);
+var io = require('socket.io')(); //(http)
 
 const port = process.env.PORT || 3000;
 
@@ -12,8 +12,17 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-http.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
 
+io.attach(server);
+
 //socket.io chat app stuff to follow
+io.on('connection', function(socket){
+    console.log('a user has connected', socket);
+
+    socket.on('disconnect', function(){
+        console.log('a user has disconnected');
+    });
+})
