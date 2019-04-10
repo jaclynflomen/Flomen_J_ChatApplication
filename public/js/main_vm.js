8d5c387ee76c9f7955a8820b433c760922d028fa
jaclynflomen <j_floman@fanshaweonline.ca>
2019-04-09 21:26:36 -0400

    notification when entering

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -5,7 +5,11 @@ const socket = io();
 function logConnect({sID, message}){ //sID, message
     console.log(sID, message);
     vm.socketID = sID;
+
+    var newUser = new Object();
+	socket.emit('chat message', { content: "A new user has entered the chat", object: newUser});
 }
+
 
 function appendMessage(message){
     vm.messages.push(message);
